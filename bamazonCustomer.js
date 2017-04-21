b6ab0e221506dc8029b44f33c65486b6f432ce5f
Glen Turner <gturner0316@yahoo.com>
2017-04-21 13:59:21 -0400

    done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,40 +11,39 @@ var itemArray = [];
 var checkOutTotal;
 
 
-  //establish connection
-  var connection = mysql.createConnection({
-    host    :'localHost',
-    user    :'root',
-    password  :'root',
-    port    :'8889',
-    database    :'Bamazon'
-  });
+//establish connection
+var connection = mysql.createConnection({
+    host: 'localHost',
+    user: 'root',
+    password: 'root',
+    port: '8889',
+    database: 'Bamazon'
+});
 
 // Check to see if connection is successful
-connection.connect(function(err){
-  if(err) throw err;
-  console.log('Connected as id ' + connection.threadId);
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected as id ' + connection.threadId);
 });
 
 var query = 'SELECT * FROM products';
 
-connection.query(query, function(error, res, field){
-console.log('****************************************************************************************'.green);
-console.table(res);
-console.log('*****************************************************************************************'.green);
-customerRequest();
+connection.query(query, function(error, res, field) {
+    console.log('****************************************************************************************'.green);
+    console.table(res);
+    console.log('*****************************************************************************************'.green);
+    customerRequest();
 });
 
 
-function customerRequest(cartTotal){
-  // prompt customer for item id and,
-  // validate their response against the number of items available
-   inquirer.prompt([
-        {
+function customerRequest(cartTotal) {
+    // prompt customer for item id and,
+    // validate their response against the number of items available
+    inquirer.prompt([{
             type: 'input',
             name: 'item_id',
             message: 'What would you like to buy? (enter item ID number)'.inverse,
-            validate: function (value) {
+            validate: function(value) {
                 var valid = !isNaN(parseFloat(value));
                 return valid || 'Please enter a number';
             },
@@ -54,28 +53,30 @@ function customerRequest(cartTotal){
             type: 'input',
             name: 'quantity',
             message: 'How many would you like to buy?'.inverse,
-            validate: function (value) {
+            validate: function(value) {
                 var valid = !isNaN(parseFloat(value));
                 return valid || 'Please enter a number';
             },
             filter: Number
         }
-    ]).then(function (answer) {
+    ]).then(function(answer) {
         // check to see if inventory of selected product is available
         var inventoryQuery = 'SELECT * FROM `products` WHERE ? AND `stock_quantity` >= ?';
-        connection.query(inventoryQuery, [{id: answer.item_id}, answer.quantity], function (err, res) {
+        connection.query(inventoryQuery, [{
+            id: answer.item_id
+        }, answer.quantity], function(err, res) {
             if (err) throw err;
             // if result of query is > 0, there is enough quantity available
             if (res.length > 0) {
                 purchase(res, answer.quantity);
-                  connection.query(query, function(error, res, field){
+                connection.query(query, function(error, res, field) {
                     console.log('************************************'.green);
                     console.table(res);
                     console.log('************************************'.green);
-                  });          
+                });
 
             } else {
-               
+
                 console.log('********************************************'.red);
                 console.log(colors.red.inverse('Sorry, we do not have enough inventory to cover your order.'));
                 console.log('********************************************'.red);
@@ -85,15 +86,15 @@ function customerRequest(cartTotal){
 
 
     }); // end .then
-    
+
 } /*End of customerRequest*/
 
-var purchase = function (purchaseItem, userQuant) {
+var purchase = function(purchaseItem, userQuant) {
     // calculate user's total
     var total = (purchaseItem[0].price * userQuant);
     var item = (purchaseItem[0].product_name);
     itemArray.push(item);
-    console.log(colors.green.inverse("Items in cart: " + " " + itemArray +  ","));
+    console.log(colors.green.inverse("Items in cart: " + " " + itemArray + ","));
 
     // array that hold all the values for the shopping cart // 
     cartArray.push(total);
@@ -108,41 +109,38 @@ var purchase = function (purchaseItem, userQuant) {
     // subtract amount purchased from stock quantity
     var newQuant = purchaseItem[0].stock_quantity - userQuant;
     // update database table `products`
-    connection.query("UPDATE `products` SET ? WHERE ?", [
-        {
+    connection.query("UPDATE `products` SET ? WHERE ?", [{
             stock_quantity: newQuant
         },
         {
             id: purchaseItem[0].id
         }
-    ], function (err, res) {
+    ], function(err, res) {
         if (err) throw err;
-       /* console.log('Great Choice!'.cyan);*/
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: 'action',
-                message: 'What would you like to do?'.inverse,
-                choices: ['Continue Shopping', 'Checkout']
-            }
-        ]).then(function (data) {
+        /* console.log('Great Choice!'.cyan);*/
+        inquirer.prompt([{
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?'.inverse,
+            choices: ['Continue Shopping', 'Checkout']
+        }]).then(function(data) {
             switch (data.action) {
                 // if user selects to continue shopping, display products
                 case 'Continue Shopping':
                     console.log(colors.green.inverse("Your balance is now: " + currentTotal));
                     console.log(colors.inverse('Here\'s what we have in stock:'));
-                    connection.query(query, function(error, res, field){
-                    console.log('**********************************************************'.cyan);
-                    console.table(res);
-                    console.log('**********************************************************'.cyan);
-                    customerRequest(cartTotal);
-                  }); 
+                    connection.query(query, function(error, res, field) {
+                        console.log('**********************************************************'.cyan);
+                        console.table(res);
+                        console.log('**********************************************************'.cyan);
+                        customerRequest(cartTotal);
+                    });
                     break;
                 case 'Checkout':
-                     function cartValue (total, num) {
+                    function cartValue(total, num) {
                         return total + num;
                     }
-                     checkOutTotal = cartArray.reduce(cartValue);
+                    checkOutTotal = cartArray.reduce(cartValue);
                     console.log('***************************************************************************************************'.blue);
                     console.table(purchaseItem, total);
                     console.log(colors.blue.inverse('Your order for ' + itemArray + ' is processing. Your total is: $' + checkOutTotal + " " + '.'));
@@ -161,41 +159,37 @@ var purchase = function (purchaseItem, userQuant) {
 }; // end purchase
 
 
-var updateSales = function (purchaseItem, totalRev) {
+var updateSales = function(purchaseItem, totalRev) {
     // updates `products` table with revenue information per item
     var salesQuery = 'SELECT * FROM `products` WHERE ?';
-    connection.query(salesQuery, [{id: purchaseItem[0].id}], function (err, res) {
+    connection.query(salesQuery, [{
+        id: purchaseItem[0].id
+    }], function(err, res) {
         if (err) throw err;
         // add new transRevenue to any existing revenue // 
         var newTotalSales = res[0].product_sales + totalRev;
-        connection.query("UPDATE `products` SET ? WHERE ?", [
-                {
-                    product_sales: newTotalSales
-                },
-                {
-                    id: res[0].id
-                }
-                
-            ], function (err, res) {
-                if (err) throw err;
+        connection.query("UPDATE `products` SET ? WHERE ?", [{
+                product_sales: newTotalSales
+            },
+            {
+                id: res[0].id
             }
-        ); // end connection.query update `products`
+
+        ], function(err, res) {
+            if (err) throw err;
+        }); // end connection.query update `products`
     }); // end connection.query salesQuery // 
 }; // end updateSales //
 
-var checkOut = function (purchaseItem, purchaseTotal,receipt ){
-  // let receiptQuery = 'SELECT * FROM `products` WHERE `stock_quantity` < 10';
-  //   connection.query(lowInvQuery, function (err, res) {
-  //     // displays table //
-  //     console.log("*********************************************************************************".inverse.red);
-  //     console.table(res);
-  //     console.log("*********************************************************************************".inverse.red);
-  //       if (err) throw err;
-  //       if (res > 10) {
-  //           console.log('All Inventory is stocked to par.'.inverse.green);
-  //       } 
+var checkOut = function(purchaseItem, purchaseTotal, receipt) {
+    // let receiptQuery = 'SELECT * FROM `products` WHERE `stock_quantity` < 10';
+    //   connection.query(lowInvQuery, function (err, res) {
+    //     // displays table //
+    //     console.log("*********************************************************************************".inverse.red);
+    //     console.table(res);
+    //     console.log("*********************************************************************************".inverse.red);
+    //       if (err) throw err;
+    //       if (res > 10) {
+    //           console.log('All Inventory is stocked to par.'.inverse.green);
+    //       } 
 };
-
-
-
-
--- manager.js
+++ manager.js
@@ -4,40 +4,38 @@ var Table = require('console.table');
 var colors = require('colors');
 
 //establish connection
-  var connection = mysql.createConnection({
-    host    :'localHost',
-    user    :'root',
-    password  :'root',
-    port    :'8889',
-    database    :'Bamazon'
-  });
-
-// Check to see if connection is successful
-connection.connect(function(err){
-  if(err) throw err;
-  console.log('Connected as id '.inverse + connection.threadId);
-  managerView();
+var connection = mysql.createConnection({
+    host: 'localHost',
+    user: 'root',
+    password: 'root',
+    port: '8889',
+    database: 'Bamazon'
 });
 
-var displayTable = function (){
-		let query = 'SELECT * FROM products';
-		connection.query(query, function(error, res, field){
-               console.log('*****************************************************'.inverse.green);
-               console.table(res);
-               console.log('*****************************************************'.inverse.green);
-         }); 
+// Check to see if connection is successful
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected as id '.inverse + connection.threadId);
+    managerView();
+});
+
+var displayTable = function() {
+    let query = 'SELECT * FROM products';
+    connection.query(query, function(error, res, field) {
+        console.log('*****************************************************'.inverse.green);
+        console.table(res);
+        console.log('*****************************************************'.inverse.green);
+    });
 };
 
-var managerView = function () {
+var managerView = function() {
     // ask user what product and how many they would like to buy
-        inquirer.prompt([
-        {
-            type: 'list',
-            name: 'next',
-            message: 'What would you like to do?',
-            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Disconnect']
-        }
-    ]).then(function (data) {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'next',
+        message: 'What would you like to do?',
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Disconnect']
+    }]).then(function(data) {
         switch (data.next) {
             case 'View Products for Sale':
                 console.log('Inventory in stock:');
@@ -67,12 +65,12 @@ var managerView = function () {
 
 var displayInventory = function() {
     let displayInvQuery = 'SELECT * FROM `products`';
-    connection.query(displayInvQuery, function (err, res) {
+    connection.query(displayInvQuery, function(err, res) {
         if (err) throw err;
         // displays table // 
-      console.log("************************************************************************.".inverse.green);
-      console.table(res);
-      console.log("************************************************************************.".inverse.green);
+        console.log("************************************************************************.".inverse.green);
+        console.table(res);
+        console.log("************************************************************************.".inverse.green);
 
         managerView();
     }); // end connection.query // 
@@ -80,29 +78,28 @@ var displayInventory = function() {
 
 var displayLowInventory = function() {
     let lowInvQuery = 'SELECT * FROM `products` WHERE `stock_quantity` < 10';
-    connection.query(lowInvQuery, function (err, res) {
-    	// displays table //
-      console.log("*********************************************************************************".inverse.red);
-      console.table(res);
-      console.log("*********************************************************************************".inverse.red);
+    connection.query(lowInvQuery, function(err, res) {
+        // displays table //
+        console.log("*********************************************************************************".inverse.red);
+        console.table(res);
+        console.log("*********************************************************************************".inverse.red);
         if (err) throw err;
         if (res > 10) {
             console.log('All Inventory is stocked to par.'.inverse.green);
-        } 
-        
+        }
+
         managerView();
     }); // end connection.query //
 }; // end displayLowInventory // 
 
-var addInventory = function() {         
-	displayTable();
+var addInventory = function() {
+    displayTable();
     console.log('Restock Inventory'.inverse.yellow);
-    inquirer.prompt([
-        {
+    inquirer.prompt([{
             type: 'input',
             name: 'productId',
             message: 'Enter product ID number of item to Restock'.inverse.yellow,
-            validate: function (value) {
+            validate: function(value) {
                 var valid = !isNaN(parseFloat(value));
                 return valid || 'Please enter a number';
             },
@@ -112,37 +109,36 @@ var addInventory = function() {
             type: 'input',
             name: 'quantity',
             message: 'Number to Restock'.inverse.yellow,
-            validate: function (value) {
+            validate: function(value) {
                 var valid = !isNaN(parseFloat(value));
                 return valid || 'Please enter a number';
             },
             filter: Number
         }
-    ]).then(function (answer) {
+    ]).then(function(answer) {
         // query database for selected item id
         var addInvQuery = 'SELECT * FROM `products` WHERE ?';
-        connection.query(addInvQuery, [{id: answer.productId}], function (err, res) {
+        connection.query(addInvQuery, [{
+            id: answer.productId
+        }], function(err, res) {
             if (err) throw err;
             // add inventory to existing stock_quantity
             var newInv = res[0].stock_quantity + answer.quantity;
-            connection.query("UPDATE `products` SET ? WHERE ?", [
-                {
+            connection.query("UPDATE `products` SET ? WHERE ?", [{
                     stock_quantity: newInv
                 },
                 {
                     id: res[0].id
                 }
-            ], function (err, res) {
+            ], function(err, res) {
                 if (err) throw err;
                 console.log('Restock successful.'.inverse.green);
-                inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'next',
-                        message: 'Please use the arrow to pick an option on the menu.',
-                        choices: ['Add More Inventory', 'Main Menu', 'Disconnect']
-                    }
-                ]).then(function (data) {
+                inquirer.prompt([{
+                    type: 'list',
+                    name: 'next',
+                    message: 'Please use the arrow to pick an option on the menu.',
+                    choices: ['Add More Inventory', 'Main Menu', 'Disconnect']
+                }]).then(function(data) {
                     switch (data.next) {
                         // if user selects to continue shopping, display products
                         case 'Add More Inventory':
@@ -184,7 +180,7 @@ var addProduct = function() {
             type: 'input',
             name: 'price',
             message: 'Price US$',
-            validate: function (value) {
+            validate: function(value) {
                 var valid = !isNaN(parseFloat(value));
                 return valid || 'Please enter a number';
             },
@@ -194,28 +190,26 @@ var addProduct = function() {
             type: 'input',
             name: 'quantity',
             message: 'Stock Quantity',
-            validate: function (value) {
+            validate: function(value) {
                 var valid = !isNaN(parseFloat(value));
                 return valid || 'Please enter a number';
             },
             filter: Number
         }
-    ]).then(function (answer) {
+    ]).then(function(answer) {
         connection.query("INSERT INTO products SET ?", {
             product_name: answer.itemName,
             department_name: answer.deptName,
             price: answer.price,
             stock_quantity: answer.quantity,
-        }, function (err, res) {
+        }, function(err, res) {
             console.log('Product added sucessfully!'.inverse.green);
-            inquirer.prompt([
-                {
-                    type: 'list',
-                    name: 'doNext',
-                    message: 'What would you like to do?',
-                    choices: ['Add Another Product', 'Main Menu', 'Disconnect']
-                }
-            ]).then(function (data) {
+            inquirer.prompt([{
+                type: 'list',
+                name: 'doNext',
+                message: 'What would you like to do?',
+                choices: ['Add Another Product', 'Main Menu', 'Disconnect']
+            }]).then(function(data) {
                 switch (data.doNext) {
                     // if user selects to continue shopping, display products //
                     case 'Add More Inventory':
@@ -235,10 +229,4 @@ var addProduct = function() {
             }); // end .then Next //
         }); // end connection.query insert to products //
     }); // end .then new product inquirer prompt //
-}; // end newProduct // 
-
-
-
-
-
-
+}; // end newProduct //
